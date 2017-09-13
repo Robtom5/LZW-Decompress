87bb8306accf5c2e1f606b3ffe00a8c797f2a7c3
Rob Thomas <robtom_5@mac.com>
2017-09-13 12:44:55 +0100

    [Add] Added iterator to file loading
    Adjusted the file loading function to act as an iterator instead.
    Adjusted the decompressing function to utilise this too.

--- lzw_decode.py
+++ lzw_decode.py
@@ -9,48 +9,55 @@
 
 def decompress_LZW(input_file_path):
 
-    compressed = load_LZW(input_file_path)
+    # compressed = load_LZW(input_file_path)
     # Build the initial dictionary
     dict_size = 256
     dictionary = [chr(i) for i in range(dict_size)]
-
-    result = w = chr(compressed.pop(0))
+    generator = load_LZW(input_file_path)
+    
+    first = True
 
     # loop through all keys
-    for k in compressed:
-        # Check for dictionary reset
-        if (dict_size >= 4096):  # 2^12 entries
-            dict_size = 256
-            dictionary = [chr(i) for i in range(dict_size)]
-        if k > dict_size:
-            raise ValueError('Invalid key, k: %s' % k)
-        elif k == dict_size:
-            entry = w + w[0]
-        else:
-            entry = dictionary[k]
+    for chunk in generator:
+        if first:
+            result = w = chr(chunk.pop(0))
+            first = False
+        for k in chunk:
+            # Check for dictionary reset
+            if (dict_size >= 4096):  # 2^12 entries
+                dict_size = 256
+                dictionary = [chr(i) for i in range(dict_size)]
+            if k > dict_size:
+                raise ValueError('Invalid key, k: %s' % k)
+            elif k == dict_size:
+                entry = w + w[0]
+            else:
+                entry = dictionary[k]
 
-        result += entry
-        dictionary.append(w + entry[0])
-        dict_size += 1
-        w = entry
-
+            result += entry
+            dictionary.append(w + entry[0])
+            dict_size += 1
+            w = entry
     return result
 
 
 def load_LZW(input_file_path):
     with open(input_file_path, mode="rb") as file:
         string = ""
-        compressed = []
+        i = 0
+        n = 12
         while True:
             byte = file.read(1)
             if not byte:
+                yield ([int(string[i:i+n], 2) for i in range(0, len(string), n)])
                 break
             # pad with leading zeros
             string += ("{0:0>8b}".format(ord(byte)))
-    # Split into list of 12 bit sections
-    n = 12
-    compressed = ([int(string[i:i+n], 2) for i in range(0, len(string), n)])
-    return compressed
+            i += 1
+            if i >= 12:
+                yield ([int(string[i:i+n], 2) for i in range(0, len(string), n)])
+                string = ""
+                i = 0
 
 if __name__ == "__main__":
     import argparse
